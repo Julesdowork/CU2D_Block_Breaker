c73d6ecbb56684898322d275add007c8ebb291e2
Julian McNeill <julesdowork@gmail.com>
2021-08-17 23:48:41 -0400

    Distance As A Vector2

--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    [Header("Configuration Parameters")]
+    // Configuration Parameters
     [SerializeField] float screenWidthInUnits = 16f;
     [SerializeField] Vector2 screenBounds;
+
 
     // Start is called before the first frame update
     void Start()
