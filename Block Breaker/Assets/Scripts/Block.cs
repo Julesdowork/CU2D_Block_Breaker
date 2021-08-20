acf477825b36f16bfbeeee30eb6b285e9673d89c
Julian McNeill <julesdowork@gmail.com>
2021-08-19 22:39:51 -0400

    PlayClipAtPoint()

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
+
     void OnCollisionEnter2D(Collision2D other)
     {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
