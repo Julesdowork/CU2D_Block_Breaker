3ccd38ea2fd6de8a55656f2520278e0ab6615886
Julian McNeill <julesdowork@gmail.com>
2021-08-19 23:52:12 -0400

    Add Player Score

--- Block Breaker/Assets/Scripts/GameManager.cs
+++ Block Breaker/Assets/Scripts/GameManager.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // Configuration Parameters
     [Range(0.25f, 4f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+
+    // State
+    [SerializeField] int currentScore = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +21,10 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
     }
 }
