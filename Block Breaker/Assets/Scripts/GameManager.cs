1c78719729abf1f54922c7d7b59041ff0e43a2e6
Julian McNeill <julesdowork@gmail.com>
2021-08-20 00:02:15 -0400

    Display Player Score

--- Block Breaker/Assets/Scripts/GameManager.cs
+++ Block Breaker/Assets/Scripts/GameManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     // Configuration Parameters
     [Range(0.25f, 4f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // State
     [SerializeField] int currentScore = 0;
@@ -15,6 +17,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         Time.timeScale = gameSpeed;
+        scoreText.text = currentScore.ToString();
     }
 
     // Update is called once per frame
@@ -26,5 +29,6 @@ public class GameManager : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 }
