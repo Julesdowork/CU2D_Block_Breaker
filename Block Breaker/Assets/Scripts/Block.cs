4f97dd8339d9b587805b8bf2486953ed42d25f65
Julian McNeill <julesdowork@gmail.com>
2021-08-20 23:52:58 -0400

    Instantiate GameObject

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject impactEffect;
 
     // Cached References
     Level level;
@@ -22,9 +23,16 @@ public class Block : MonoBehaviour
 
 	private void DestroyBlock()
 	{
+        FindObjectOfType<GameSession>().AddToScore();
 		AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        TriggerImpactEffect();
 		Destroy(gameObject);
 		level.BlockDestroyed();
-        FindObjectOfType<GameSession>().AddToScore();
 	}
+
+    private void TriggerImpactEffect()
+    {
+        GameObject effect = Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effect, 2f);
+    }
 }
